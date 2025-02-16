b498e3f5078103226de03c26dc060a37974cbd66
Sean Sell <rough68fish@yahoo.com>
2025-02-16 15:07:36 -0500

    this version matches suggested but doesn't work

--- src/extension.ts
+++ src/extension.ts
@@ -58,7 +58,7 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 class HelloWorldViewProvider implements vscode.WebviewViewProvider {
-	public static readonly viewType = 'helloworld.helloworldView';
+	public static readonly viewType = "helloworldView";
   
 	constructor(private readonly extensionUri: vscode.Uri) {}
   
@@ -72,21 +72,54 @@ class HelloWorldViewProvider implements vscode.WebviewViewProvider {
 		localResourceRoots: [this.extensionUri]
 	  };
   
-	  webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
+	  this.updateWebviewContent(webviewView);
 	}
   
-	private getHtmlForWebview(webview: vscode.Webview): string {
-	  return `<!DOCTYPE html>
-		<html lang="en">
-		<head>
-		  <meta charset="UTF-8">
-		  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		  <title>Hello World</title>
-		</head>
-		<body>
-		  <h1>Hello World View</h1>
-		</body>
-		</html>`;
+	private updateWebviewContent(webviewView: vscode.WebviewView) {
+		const config = vscode.workspace.getConfiguration('helloworld');
+		const requirementsDirectory = config.get<string>('requirementsDirectory', './docs/requirements');
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+	
+		if (!workspaceFolders) {
+		  webviewView.webview.html = this.getHtmlForWebview([], 'No workspace is currently open.');
+		  return;
+		}
+	
+		const workspacePath = workspaceFolders[0].uri.fsPath;
+		const requirementsPath = path.join(workspacePath, requirementsDirectory);
+	
+		if (fs.existsSync(requirementsPath)) {
+		  fs.readdir(requirementsPath, (err, files) => {
+			if (err) {
+			  webviewView.webview.html = this.getHtmlForWebview([], `Error reading directory: ${err.message}`);
+			  return;
+			}
+			webviewView.webview.html = this.getHtmlForWebview(files);
+		  });
+		} else {
+		  webviewView.webview.html = this.getHtmlForWebview([], `Requirements Directory '${requirementsDirectory}' not found.`);
+		}
+	}
+
+	private getHtmlForWebview(files: string[], errorMessage: string = ''): string {
+		const fileList = files.map(file => `<li>${file}</li>`).join('');
+		const errorHtml = errorMessage ? `<p style="color: red;">${errorMessage}</p>` : '';
+	
+		return `<!DOCTYPE html>
+		  <html lang="en">
+		  <head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Hello World</title>
+		  </head>
+		  <body>
+			<h1>Hello World View</h1>
+			${errorHtml}
+			<ul>
+			  ${fileList}
+			</ul>
+		  </body>
+		  </html>`;
 	}
 }
 
